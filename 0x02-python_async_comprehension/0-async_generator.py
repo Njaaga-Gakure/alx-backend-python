d235e66cc2a38fdf25e5c83fa8263c06588cca3c
Njaaga-Gakure <njaagagakure@gmail.com>
2023-08-08 10:04:07 +0000

    Fix type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 
 
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Create an async generator that yield random values."""
     for _ in range(10):
         await asyncio.sleep(1)
